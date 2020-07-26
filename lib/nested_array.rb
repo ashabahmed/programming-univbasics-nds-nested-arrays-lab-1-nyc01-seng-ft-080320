6277a31140a113b0605d8eda3f7217d89d708239
Ashab Ahmed <ashabahmed7@gmail.com>
2020-07-26 23:38:04 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,7 +35,7 @@ def sorted_matrix
   # However, this time, sort each internal array alphabetically by the first character
    
    
-  produce_sorted = CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort
+  produce_sorted = [CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort]
     
   produce_sorted
 end
